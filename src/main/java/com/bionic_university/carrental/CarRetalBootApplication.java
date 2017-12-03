7fdf9607ed8523d3a9fec57707af679ebf1b7d18
Sowji <Sowji>
2017-12-03 14:57:57 +0530

    test

---
+++ src/main/java/com/bionic_university/carrental/CarRetalBootApplication.java
@@ -0,0 +1,12 @@
+package com.bionic_university.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRetalBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRetalBootApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bionic_university/carrental/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bionic_university.carrental;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarRetalBootApplication.class);
+	}
+
+}
